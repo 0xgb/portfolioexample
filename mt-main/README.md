82977255792c68b34d9aaa5f0fbc06c762751156
0xgb <157587438+0xgb@users.noreply.github.com>
2024-03-28 22:13:28 -0300

    start project
    first commit

---
+++ mt-main/README.md
@@ -0,0 +1,19 @@
+# How run?
+
+To run this project you go to directory, open the terminal and run 
+
+```
+  sudo apt install python3-flask
+
+```
+
+# Next step
+
+Edit how you want so... RUN, forest RUN!
+
+```
+ flask run
+ 
+```
+
+Enjoy this minimal portfolio!   
---
+++ mt-main/portfolio/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/work')
+def work():
+    return render_template('work.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
